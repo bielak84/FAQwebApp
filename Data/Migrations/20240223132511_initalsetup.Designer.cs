499f0775168f9c9732bbdf86d5da74b4d626891f
bielak84 <bielak84@xxx.xxx>
2024-02-23 14:26:06 +0100

    add-migrations "initlsetup"

--- Data/Migrations/20240223132511_initalsetup.Designer.cs
+++ Data/Migrations/20240223132511_initalsetup.Designer.cs
@@ -4,6 +4,7 @@ using FAQwebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FAQwebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240223132511_initalsetup")]
+    partial class initalsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
