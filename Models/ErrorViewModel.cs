adad0d433b02fd497f64f860390311cdf7fb83c2
bielak84 <bielak84@xxx.xxx>
2024-02-23 14:01:48 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FAQwebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/FAQ.cs
@@ -0,0 +1,24 @@
+﻿//prop
+//ctor
+namespace FAQwebApp.Models
+{
+    // Klasa reprezentująca model FAQ 
+    public class FAQ
+    {
+        // Identyfikator pytania
+        public int id { get; set; }
+
+        // Treść pytania
+        public string Question { get; set; }
+
+        // Treść odpowiedzi
+        public string Answer { get; set; }
+
+        // Konstruktor domyślny
+        public FAQ()
+        {
+            // Pusta implementacja, ponieważ nie wymaga dodatkowej logiki w konstruktorze
+        }
+    }
+}
+
