4e60f8dbfe85bae7bf4de52cffe9f4ec2c3140e4
bielak84 <bielak84@xxx.xxx>
2024-02-23 14:16:29 +0100

    dodano komentarze do kodu

--- Controllers/FAQsController.cs
+++ Controllers/FAQsController.cs
@@ -10,22 +10,26 @@ using FAQwebApp.Models;
 
 namespace FAQwebApp.Controllers
 {
+    // Kontroler obsługujący operacje związane z pytaniami i odpowiedziami (FAQ)
     public class FAQsController : Controller
     {
         private readonly ApplicationDbContext _context;
 
+        // Konstruktor kontrolera, który przyjmuje kontekst bazy danych
         public FAQsController(ApplicationDbContext context)
         {
             _context = context;
         }
 
         // GET: FAQs
+        // Akcja zwracająca widok zawierający listę wszystkich FAQ
         public async Task<IActionResult> Index()
         {
             return View(await _context.FAQ.ToListAsync());
         }
 
         // GET: FAQs/Details/5
+        // Akcja zwracająca widok z danymi szczegółowymi dla konkretnego FAQ
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -33,8 +37,10 @@ namespace FAQwebApp.Controllers
                 return NotFound();
             }
 
+            // Pobranie konkretnego FAQ o podanym identyfikatorze
             var fAQ = await _context.FAQ
                 .FirstOrDefaultAsync(m => m.id == id);
+
             if (fAQ == null)
             {
                 return NotFound();
@@ -44,28 +50,37 @@ namespace FAQwebApp.Controllers
         }
 
         // GET: FAQs/Create
+        // Akcja zwracająca widok do tworzenia nowego FAQ
         public IActionResult Create()
         {
             return View();
         }
 
         // POST: FAQs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Akcja obsługująca dodawanie nowego FAQ do bazy danych
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("id,Question,Answer")] FAQ fAQ)
         {
+            // Sprawdzenie poprawności modelu przed dodaniem do bazy danych
             if (ModelState.IsValid)
             {
+                // Dodanie nowego FAQ do kontekstu bazy danych
                 _context.Add(fAQ);
+
+                // Zapisanie zmian w bazie danych
                 await _context.SaveChangesAsync();
+
+                // Przekierowanie do widoku listy FAQ po dodaniu nowego elementu
                 return RedirectToAction(nameof(Index));
             }
+
+            // Jeśli model nie jest poprawny, zwróć widok z błędami
             return View(fAQ);
         }
 
         // GET: FAQs/Edit/5
+        // Akcja zwracająca widok do edycji konkretnego FAQ
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -73,7 +88,9 @@ namespace FAQwebApp.Controllers
                 return NotFound();
             }
 
+            // Pobranie konkretnego FAQ o podanym identyfikatorze
             var fAQ = await _context.FAQ.FindAsync(id);
+
             if (fAQ == null)
             {
                 return NotFound();
@@ -82,26 +99,31 @@ namespace FAQwebApp.Controllers
         }
 
         // POST: FAQs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Akcja obsługująca edycję konkretnego FAQ w bazie danych
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("id,Question,Answer")] FAQ fAQ)
         {
+            // Sprawdzenie, czy identyfikator FAQ w ścieżce URL zgadza się z identyfikatorem FAQ w modelu
             if (id != fAQ.id)
             {
                 return NotFound();
             }
 
+            // Sprawdzenie poprawności modelu przed zapisaniem zmian w bazie danych
             if (ModelState.IsValid)
             {
                 try
                 {
+                    // Aktualizacja FAQ w kontekście bazy danych
                     _context.Update(fAQ);
+
+                    // Zapisanie zmian w bazie danych
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
+                    // Obsługa błędów związanych z równoległym dostępem do danych
                     if (!FAQExists(fAQ.id))
                     {
                         return NotFound();
@@ -111,12 +133,17 @@ namespace FAQwebApp.Controllers
                         throw;
                     }
                 }
+
+                // Przekierowanie do widoku listy FAQ po zapisaniu zmian
                 return RedirectToAction(nameof(Index));
             }
+
+            // Jeśli model nie jest poprawny, zwróć widok z błędami
             return View(fAQ);
         }
 
         // GET: FAQs/Delete/5
+        // Akcja zwracająca widok do potwierdzenia usunięcia konkretnego FAQ
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -124,8 +151,10 @@ namespace FAQwebApp.Controllers
                 return NotFound();
             }
 
+            // Pobranie konkretnego FAQ o podanym identyfikatorze
             var fAQ = await _context.FAQ
                 .FirstOrDefaultAsync(m => m.id == id);
+
             if (fAQ == null)
             {
                 return NotFound();
@@ -135,20 +164,28 @@ namespace FAQwebApp.Controllers
         }
 
         // POST: FAQs/Delete/5
+        // Akcja obsługująca potwierdzenie usunięcia konkretnego FAQ z bazy danych
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            // Pobranie konkretnego FAQ o podanym identyfikatorze
             var fAQ = await _context.FAQ.FindAsync(id);
+
             if (fAQ != null)
             {
+                // Usunięcie FAQ z kontekstu bazy danych
                 _context.FAQ.Remove(fAQ);
             }
 
+            // Zapisanie zmian w bazie danych
             await _context.SaveChangesAsync();
+
+            // Przekierowanie do widoku listy FAQ po usunięciu
             return RedirectToAction(nameof(Index));
         }
 
+        // Metoda sprawdzająca istnienie FAQ o określonym identyfikatorze
         private bool FAQExists(int id)
         {
             return _context.FAQ.Any(e => e.id == id);
